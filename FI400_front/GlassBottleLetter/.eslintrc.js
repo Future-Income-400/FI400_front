4e48fad41b0a23caa0c420d2eef7fcb24ee83e1f
MuMU0617 <a83364867@gmail.com>
2023-08-06 16:07:32 +0900

    Test Commit

--- FI400_front/GlassBottleLetter/.eslintrc.js
+++ FI400_front/GlassBottleLetter/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rulse: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
