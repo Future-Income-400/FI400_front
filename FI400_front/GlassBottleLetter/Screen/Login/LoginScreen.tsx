a4e51b92c2e5bb2cbb0ad64415374b9ecf5d7c23
lij0825 <dldlswns890@gmail.com>
2023-08-16 23:45:22 +0900

    chore link

--- FI400_front/GlassBottleLetter/Screen/Login/LoginScreen.tsx
+++ FI400_front/GlassBottleLetter/Screen/Login/LoginScreen.tsx
@@ -34,14 +34,9 @@ var keyHash = Utility.getKeyHash(this)
 */
 
   const kakaoButtonPress = async () => {
-    try {
-      const response = await Axios.get(`/info/oauth2/kakao/client-id`); // 링크 들어가야 됨
-      const loginUrl = response.data.uri;
-    } catch (err) {
-      console.error('err', err);
-    }
     // 카카오 로그인 버튼 눌렀을 때 처리할 로직을 여기에 추가
     // Alert.alert('카카오버튼 잘 작동');
+    Linking.openURL(link);
   };
 
   const instaButtonPress = () => {
