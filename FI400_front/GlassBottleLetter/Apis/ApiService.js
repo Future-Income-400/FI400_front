f78171df88b9828631d450c208dd1b7f3fad3f31
lij0825 <dldlswns890@gmail.com>
2023-08-16 22:35:47 +0900

    chore ApiService.js
    ApiService.js 이름 쓰기 쉽게 수정

--- FI400_front/GlassBottleLetter/Apis/ApiService.js
+++ FI400_front/GlassBottleLetter/Apis/ApiService.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const SEVER_ADDRESS = 'http://bottleletter.com/api';
 
-const apiService = axios.create({
+export const Axios = axios.create({
   baseURL: SEVER_ADDRESS,
   headers: {},
 });
-
-export default apiService;
