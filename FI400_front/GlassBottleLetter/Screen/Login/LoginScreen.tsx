039c840421b5960fc4bd834cf71a33720757a78a
lij0825 <dldlswns890@gmail.com>
2023-08-16 22:39:23 +0900

    feat 카카오 로그인 함수 추가

--- FI400_front/GlassBottleLetter/Screen/Login/LoginScreen.tsx
+++ FI400_front/GlassBottleLetter/Screen/Login/LoginScreen.tsx
@@ -1,6 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import LoginButton from '../../Component/LoginButton';
-import {StyleSheet, Alert, View, ImageBackground, Linking,Text} from 'react-native';
+import {
+  StyleSheet,
+  Alert,
+  View,
+  ImageBackground,
+  Linking,
+  Text,
+} from 'react-native';
+import {Axios} from '../../Apis/ApiService';
 
 // LoginScreen.
 const LoginScreen = ({navigation, route}: any) => {
@@ -11,18 +19,30 @@ const LoginScreen = ({navigation, route}: any) => {
   const instaImagePath =
     'https://raw.githubusercontent.com/Future-Income-400/FI400_front/master/FI400_front/GlassBottleLetter/Image/instagram_login_large_wide.png';
 
-  const REST_API_KEY ='3ea849343e7ba259ab812b860ec4770a';
-  const REDIRECT_URI ='http://localhost:8081/kakao/callback';
+  const REST_API_KEY = '3ea849343e7ba259ab812b860ec4770a';
+  const REDIRECT_URI = 'http://localhost:8081/kakao/callback';
 
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   // URL 문자열을 파싱하여 파라미터 값을 추출
   // const code = new URL(dococument.location.toString()).searchParams.get('code');
 
+  /* Kakao SDK로 디버그, 릴리즈 키 해시 확인
+import com.kakao.sdk.common.util.Utility
+
+var keyHash = Utility.getKeyHash(this)
+*/
+
   const kakaoButtonPress = () => {
+    const kakaoLogin = async () => {
+      try {
+        const response = await Axios.get(`https://`);
+      } catch (err) {
+        console.error('err', err);
+      }
+    };
     // 카카오 로그인 버튼 눌렀을 때 처리할 로직을 여기에 추가
     // Alert.alert('카카오버튼 잘 작동');
-    Linking.openURL(link);
   };
 
   const instaButtonPress = () => {
