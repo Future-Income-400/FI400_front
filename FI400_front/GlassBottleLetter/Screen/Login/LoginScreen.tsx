024eabb42b73d49b70f36887e484dd6b85dcf910
MuMU0617 <a83364867@gmail.com>
2023-08-30 22:25:01 +0900

    feat : 인가코드를 통한 API 요청기능 추가

--- FI400_front/GlassBottleLetter/Screen/Login/LoginScreen.tsx
+++ FI400_front/GlassBottleLetter/Screen/Login/LoginScreen.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginButton from '../../Component/LoginButton';
 import {
   StyleSheet,
   View,
   ImageBackground,
   Linking,
+  Alert
 } from 'react-native';
 import queryString from 'query-string';
-import { useRoute } from '@react-navigation/native';
+import WebView from 'react-native-webview';
+import jwt from 'jsonwebtoken';
 
 // LoginScreen.
 const LoginScreen = ({navigation, route}: any) => {
+  const [showWebView, setShowWebView] = useState(false);
+  const [authorizationCode, setAuthorizationCode] = useState('');
+
   // 카카오 로그인 버튼 이미지 파일의 Raw 경로
   const kakaoImagePath =
     'https://raw.githubusercontent.com/Future-Income-400/FI400_front/master/FI400_front/GlassBottleLetter/Image/kakao_login_large_wide.png';
@@ -23,30 +28,58 @@ const LoginScreen = ({navigation, route}: any) => {
 
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
-  // const Route = useRoute();
-
-  // console.log(Route.name);
-
   // 카카오 로그인 버튼 눌렀을 때 처리할 로직을 여기에 추가
   const kakaoButtonPress = async () => {
-    Linking.openURL(link);
-
-    const url = "http://localhost:8080/kakao/callback?code=9ca1up-AbgMQGXpCkpcFbrVvVPOyel2uyHI8g3Gdrf8IVdI-7v4RYZOPt_pvfdbpCvLDCwo9cpgAAAGKGFt9hQ";
-
-    const parsedUrl = queryString.parseUrl(url);
-    const code: string | null = (parsedUrl.query as { [key: string]: string | null }).code || null;
-
-    if (code) {
-      console.log(code);  // 여기서 코드 값을 확인할 수 있어용~!
-    } else {
-      console.log('코드가 없어용 ㅠㅅㅠ');
-    }
-    
+    setShowWebView(true);
   };
 
   // 인스타 로그인 버튼 눌렀을 때 처리할 로직을 여기에 추가
   const instaButtonPress = () => {
     navigation.navigate('MainScreen');
+  };
+
+  const handleWebViewNavigation = (navState: any) => {
+    // 웹뷰의 네비게이션 상태가 변경될 때 처리할 로직 추가
+    // navState에는 현재 URL 정보 등이 포함되어 있음.
+
+    // 인가 코드 추출
+    const parsedUrl = queryString.parseUrl(navState.url);
+    const code: string | null = (parsedUrl.query as { [key: string]: string | null }).code || null;
+
+    console.log(code);
+
+    if (code) {
+      // 인가 코드를 백엔드 API에 전송
+      sendAuthorizationCodeToBackend(code);
+
+      // Alert.alert('인가코드: \n' + code);
+
+      setAuthorizationCode(code);
+      setShowWebView(false); // 웹뷰 닫기
+    }
+  };
+
+  const sendAuthorizationCodeToBackend = async (code: string) => {
+    try {
+      const response = await fetch('http://localhost:8080/user/saveuser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code }),
+      });
+
+      // 응답 처리
+      if (response.ok) {
+        // 요청이 성공한 경우
+        console.log('인가 코드 전송 성공');
+      } else {
+        // 요청이 실패한 경우
+        console.error('인가 코드 전송 실패');
+      }
+    } catch (error) {
+      // console.error('네트워크 오류:', error);
+    }
   };
 
   return (
@@ -75,6 +108,12 @@ const LoginScreen = ({navigation, route}: any) => {
           }}
         />
       </View>
+      {showWebView && (
+          <WebView
+            source={{ uri: link }}
+            onNavigationStateChange={handleWebViewNavigation}
+          />
+        )}
     </ImageBackground>
   );
 };
